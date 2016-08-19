148c338f9589517156615c773fc4a98cffb15c3f
likeshan168 <likeshan168@163.com>
2016-08-19 09:05:03 +0800

    添加bootstrap sass源码

--- mvcMovie/wwwroot/_references.js
+++ mvcMovie/wwwroot/_references.js
@@ -1,6 +1,7 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
+/// <reference path="lib/bootstrap-sass-3.3.5/assets/javascripts/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
